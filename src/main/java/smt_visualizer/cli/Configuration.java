b92bfbf774b5f925d85ac77225e4dc63dd29e6d2
tzwickl <thomas.zwickl@gmail.com>
2016-03-02 22:38:35 +0100

    Removed unused code.

--- src/main/java/smt_visualizer/cli/Configuration.java
+++ src/main/java/smt_visualizer/cli/Configuration.java
@@ -6,9 +6,4 @@ public class Configuration implements Serializable {
 
 	private static final long serialVersionUID = -4273619306317175899L;
 
-	public static String CSV_PATH;
-	public static String CASSANDRA_IP = "192.168.22.187";
-	public static String CASSANDRA_TABLE = "measurement";
-	public static String CASSANDRA_KEYSPACE = "retit";
-
 }
